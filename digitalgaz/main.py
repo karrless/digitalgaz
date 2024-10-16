a832016ae3bf1adccc797aaf6f114a6b75a5e80d
karrless <konstantin.geydarov@gmail.com>
2024-10-17 02:17:14 +0300

    hothotfix

--- digitalgaz/main.py
+++ digitalgaz/main.py
@@ -5,8 +5,6 @@ load_dotenv()
 
 from digitalgaz.app import app
 
-from digitalgaz.app import app
-
 def main():
     
     logger.remove()
@@ -14,7 +12,7 @@ def main():
                level="DEBUG")
     logger.info("Старт скрипта")
     
-    uvicorn.run("digitalgaz.app:app", host="localhost", port=3005, reload=True)
+    uvicorn.run("digitalgaz.main:app", host="localhost", port=3005, reload=True)
 
 if __name__ == '__main__':
     main()
